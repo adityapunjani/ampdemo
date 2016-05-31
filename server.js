160ed64be7b197e5379a1d35591e9ee03ebb75c6
Aditya Punjani <aditya.punjani@flipkart.com>
2016-06-01 04:07:42 +0530

    changes

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+"use strict";
 var express = require('express'),
     app = express(),
     http = require('http'),
     compression = require('compression'),
     fs = require('fs'),
-    bodyParser = require('body-parser'),
+    path = require('path'),
     https = require('https'),
     port = process.env.PORT || 8080;
 
@@ -14,15 +15,46 @@ var allowCrossDomain = function(req, res, next) {
     next();
 }
 
-app.use(allowCrossDomain);
+require('marko/hot-reload').enable();
+var template = require('marko').load(require.resolve('./template.marko'));
+//app.use(allowCrossDomain);
 app.use(compression());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
+
+
+require('fs').watch(__dirname, function (event, filename) {
+    if (/\.marko$/.test(filename)) {
+        // Resolve the filename to a full template path:
+        var templatePath = path.join(__dirname, filename);
+
+        console.log('Marko template modified: ', templatePath);
+
+        // Pass along the *full* template path to marko
+        require('marko/hot-reload').handleFileModified(templatePath);
+    }
+});
+
+app.get('/search.html', function (req, res) {
+    res.end(fs.readFileSync(path.join('search.html')));
+});
+
+app.get('/robots.txt', function (req, res) {
+    res.write('User-agent: * ');
+    res.end('Allow: /');
+});
 
 app.get('/*', function (req, res) {
-    res.end(fs.readFileSync('index.html'));
+    template.render({
+        name: 'Moto g',
+        count: 30
+    },
+    function(err, output) {
+        if (err) {
+            console.error('Rendering failed');
+            return;
+        }
+ 
+        res.end(output);
+    });
 });
 
 http.createServer(app).listen(port, function (err) {
